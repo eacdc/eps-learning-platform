37335c7be4325b72c6e016eb9603874321c919f8
eacdc <ea@cdcprinters.com>
2026-08-05 12:18:32 +0530

    update

--- lib/constants/grade_list.dart
+++ lib/constants/grade_list.dart
@@ -11,16 +11,14 @@ class Grade {
 }
 
 final List<Grade> gradeList = [
-  Grade(value: 1, name: "Niveau 1"),
-  Grade(value: 2, name: "Niveau 2"),
-  Grade(value: 3, name: "Niveau 3"),
-  Grade(value: 4, name: "Niveau 4"),
-  Grade(value: 5, name: "Niveau 5"),
-  Grade(value: 6, name: "Niveau 6"),
-  Grade(value: 7, name: "Niveau 7"),
-  Grade(value: 8, name: "Niveau 8"),
-  Grade(value: 9, name: "Niveau 9"),
-  Grade(value: 10, name: "Niveau 10"),
-  Grade(value: 11, name: "Niveau 11"),
-  Grade(value: 12, name: "Niveau 12"),
+  Grade(value: 1, name: "CP1"),
+  Grade(value: 2, name: "CE1"),
+  Grade(value: 3, name: "CM1"),
+  Grade(value: 4, name: "6e"),
+  Grade(value: 5, name: "5e"),
+  Grade(value: 6, name: "4e"),
+  Grade(value: 7, name: "3e"),
+  Grade(value: 8, name: "2e"),
+  Grade(value: 9, name: "1re"),
+  Grade(value: 10, name: "Tle"),
 ];
